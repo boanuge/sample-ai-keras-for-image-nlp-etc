1a157b8552cd2ec251c378e0b26509092a40a50c
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-03-07 08:08:14 +0900

    Add files via upload

---
+++ yolo3_config.py
@@ -0,0 +1,19 @@
+import os
+
+root_directory_windows = "C:\\AI\\ai-service" # os.getcwd() + "/"
+root_directory_ubuntu = "/home/ai-service @ image"
+
+# YOLO_v3
+yolo_image_path = "images_test/"
+yolo_error_log_disable = True
+yolo_future_error_log_disable = True
+
+class YOLO_class_default:
+    def __init__(self):
+        self.model_path = 'output/yolo3_weights_trained_1910_epoch_100.h5'
+        self.anchors_path = 'yolo_anchors.txt'  # 디폴트값 사용
+        self.classes_path = 'yolo_classes.txt'  # 디폴트값 사용
+        self.score = 0.3  # 디폴트값 사용
+        self.iou = 0.45  # 디폴트값 사용
+        self.model_image_size = (416, 416)  # 디폴트값 사용
+        self.gpu_num = 0,  # Only CPU # 디폴트값 사용
