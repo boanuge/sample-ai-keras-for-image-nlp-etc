c5a122fe30bbbbe4135b8c3a57d97c922c000962
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-03-21 08:08:24 +0900

    Update yolo3_config.py

--- yolo3_config.py
+++ yolo3_config.py
@@ -3,14 +3,16 @@ import os
 root_directory_windows = "C:\\AI\\ai-service" # os.getcwd() + "/"
 root_directory_ubuntu = "/home/ai-service @ image"
 
+model_path = "./output/"
+yolo_image_path = "./images_test/"
+
 # YOLO_v3
-yolo_image_path = "images_test/"
 yolo_error_log_disable = True
 yolo_future_error_log_disable = True
 
 class YOLO_class_default:
     def __init__(self):
-        self.model_path = 'output/yolo3_weights_trained_1910_epoch_100.h5'
+        self.model_path = 'output/yolo3_weights_trained_epoch_100.h5'
         self.anchors_path = 'yolo_anchors.txt'  # 디폴트값 사용
         self.classes_path = 'yolo_classes.txt'  # 디폴트값 사용
         self.score = 0.3  # 디폴트값 사용
