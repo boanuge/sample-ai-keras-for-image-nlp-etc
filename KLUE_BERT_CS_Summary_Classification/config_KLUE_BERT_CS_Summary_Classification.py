0b85c675b2a690b3d5295be2977ec47b63be1da8
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-03-27 15:17:55 +0900

    Update config_KLUE_BERT_CS_Summary_Classification.py

--- KLUE_BERT_CS_Summary_Classification/config_KLUE_BERT_CS_Summary_Classification.py
+++ KLUE_BERT_CS_Summary_Classification/config_KLUE_BERT_CS_Summary_Classification.py
@@ -1,7 +1,7 @@
 import os
 
 root_directory_windows = "C:\\AI\\ai-service-cs-category"
-root_directory_ubuntu = "/home/gbike/AI_live/ai-service-cs-category @ live"
+root_directory_ubuntu = "/home/user/AI_live/ai-service-cs-category @ live"
 host_num = '0.0.0.0'
 port_num = 5000
 
