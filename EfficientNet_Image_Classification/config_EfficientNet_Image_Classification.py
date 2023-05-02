b8f546ee2c638ccae8f52b14ac773ee80343be3e
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-05-02 13:25:34 +0900

    Update config_EfficientNet_Image_Classification.py

--- EfficientNet_Image_Classification/config_EfficientNet_Image_Classification.py
+++ EfficientNet_Image_Classification/config_EfficientNet_Image_Classification.py
@@ -1,7 +1,8 @@
 import os
 
 root_directory_windows = "C:\\AI\\ai-service-image-classification"
-root_directory_ubuntu = "/home/user/AI_live/ai-service-cs-comment @ live"
+#root_directory_ubuntu = "/home/user/AI_live/ai-service-image-classification"
+root_directory_ubuntu = os.getcwd()
 host_num = '0.0.0.0'
 port_num = 5000
 
