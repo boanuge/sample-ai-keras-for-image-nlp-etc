1712eb56a272ae469f27e6eb0af4d1fec0e5c517
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-05-02 13:24:02 +0900

    Update config_ai_service_image.py

--- Object_Detection_using_YOLOv3/ai-service-image/config_ai_service_image.py
+++ Object_Detection_using_YOLOv3/ai-service-image/config_ai_service_image.py
@@ -1,4 +1,5 @@
 root_directory_windows = "C:\\AI\\ai-service-image @ 2023-05-01"
-root_directory_ubuntu = "/home/user/ai-service-image"
+#root_directory_ubuntu = "/home/user/ai-service-image"
+root_directory_ubuntu = os.getcwd()
 host_num = '0.0.0.0'
 port_num = 5001
