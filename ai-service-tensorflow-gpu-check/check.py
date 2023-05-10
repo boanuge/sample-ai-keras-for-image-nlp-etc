c22278123026ea92b612b0c7283a79ff1245a06c
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-05-10 14:28:57 +0900

    Add files via upload

--- ai-service-tensorflow-gpu-check/check.py
+++ ai-service-tensorflow-gpu-check/check.py
@@ -3,6 +3,20 @@ import pandas as pd
 from tensorflow import keras
 from tensorflow.keras import layers
 from matplotlib import pyplot as plt
+import tensorflow as tf
+# GPU에 할당되는 전체 메모리 크기를 제한
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+  try:
+    tf.config.experimental.set_virtual_device_configuration(
+        gpus[0],
+        [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=1024)])
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+  except RuntimeError as e:
+    # Virtual devices must be set before GPUs have been initialized
+    print(e)
 
 master_url_root = "https://raw.githubusercontent.com/numenta/NAB/master/data/"
 
