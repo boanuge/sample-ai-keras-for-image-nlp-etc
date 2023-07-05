cf09fd8533f608f225e3ea5be4782d7d1de15829
The Bible-smith Project <boanuge@users.noreply.github.com>
2023-07-05 14:32:51 +0900

    Update model_Image_Classification.py

--- EfficientNet_Image_Classification/ai-service-image/model_Image_Classification.py
+++ EfficientNet_Image_Classification/ai-service-image/model_Image_Classification.py
@@ -17,10 +17,16 @@ def get_class(image):
     global loaded_model
 
     new_size = (456, 456) # EfficientNetB5
+
     img_resized = image.resize(new_size)
-    img_vectorized = tf.keras.preprocessing.image.img_to_array(img_resized)
-    img_vectorized = tf.keras.applications.efficientnet.preprocess_input(img_vectorized)
+    img_vectorized = (np.array(img_resized) / 255.0).astype('float32')
     img_vectorized = np.expand_dims(img_vectorized, axis=0)
+    
+    #img_resized = image.resize(new_size)
+    #img_vectorized = tf.keras.preprocessing.image.img_to_array(img_resized)
+    #img_vectorized = tf.keras.applications.efficientnet.preprocess_input(img_vectorized)
+    #img_vectorized = np.expand_dims(img_vectorized, axis=0)
+    
     predicted_result = loaded_model.predict(img_vectorized, verbose=0)
 
     return predicted_result
